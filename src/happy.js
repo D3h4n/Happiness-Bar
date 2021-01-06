fc9f6ba6ca1531086b57be283a81d9adbb23437a
D3h4n <shawn2012tt@live.com>
2021-01-05 22:44:13 -0400

    Initial commit

---
+++ src/happy.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class happy extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
